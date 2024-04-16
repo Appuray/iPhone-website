0efa0f163e3da486bc2d19722af418bbe3cb15ab
Ray204-code <144940367+Ray204-code@users.noreply.github.com>
2024-04-17 04:11:21 +0530

    Added sections

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -30,7 +30,7 @@ const ModelView = ({
         ref={controlRef}
         enableZoom={false}
         enablePan={false}
-        rotateSpeed={0.4}
+        rotateSpeed={0.9}
         target={new THREE.Vector3(0, 0, 0)}
         onEnd={() => setrotationState(controlRef.current.getAzimuthalAngle())}
       />
